f7067a08adf8e0f9f0f6007863d28312c6d64f5b
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-16 02:04:27 +0530

    update hover on icon

--- src/components/Project/Projectitem.js
+++ src/components/Project/Projectitem.js
@@ -4,6 +4,7 @@ import "./ProjectItemCSS.css";
 const Projectitem = (props) => {
   const { sourceCodeLink, deployedLink, name, description, languages } = props;
   const [showFullDescription, setShowFullDescription] = useState(false);
+  const [hoveredIcon, setHoveredIcon] = useState("");
 
   const toggleDescription = () => {
     setShowFullDescription(!showFullDescription);
@@ -14,16 +15,46 @@ const Projectitem = (props) => {
       ? description.substring(0, 250) + "..."
       : description;
 
+  const handleMouseEnter = (iconName) => {
+    setHoveredIcon(iconName);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIcon("");
+  };
+
   return (
     <>
       <div className="project-item">
         <div className="project-links">
-          <a href={sourceCodeLink} target="_blank" rel="noopener noreferrer">
-            <i className="fa-solid fa-code-branch project-icon"></i>
-          </a>
-          <a href={deployedLink} target="_blank" rel="noopener noreferrer">
-            <i className="fa-solid fa-globe project-icon"></i>
-          </a>
+          <div className="icon-container">
+            <a
+              href={sourceCodeLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              onMouseEnter={() => handleMouseEnter("View Source Code")}
+              onMouseLeave={handleMouseLeave}
+            >
+              <i className="fa-solid fa-code-branch project-icon"></i>
+            </a>
+            {hoveredIcon === "View Source Code" && (
+              <span className="icon-tooltip">View Source Code</span>
+            )}
+          </div>
+          <div className="icon-container">
+            <a
+              href={deployedLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              onMouseEnter={() => handleMouseEnter("View Deployed Version")}
+              onMouseLeave={handleMouseLeave}
+            >
+              <i className="fa-solid fa-globe project-icon"></i>
+            </a>
+            {hoveredIcon === "View Deployed Version" && (
+              <span className="icon-tooltip">View Deployed Version</span>
+            )}
+          </div>
         </div>
         <h3 className="project-name">{name}</h3>
         <p className="project-description">
